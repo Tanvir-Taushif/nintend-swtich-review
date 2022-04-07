d6ab52587ab6bf0e890549bd39829009a2ed4a57
Tanvir Taushif <tanvirpial9961@gmail.com>
2022-04-08 01:02:37 +0600

    Navbar Initialization Commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,17 +3,26 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 import { MenuIcon } from '@heroicons/react/solid';
 const Header = () => {
+    const clickHandler=()=>{
+        if(document.getElementById('navManu').style.display==='none'){
+            document.getElementById('navManu').style.display='block';
+        }
+        else{
+            document.getElementById('navManu').style.display='none';
+        }
+    }
     return (
         <div>
-            <nav id='NavBar' className='my-4'>
-                <div className='navManu'>
+            <nav id='NavBar' className='my-4 d-flex justify-content-center '>
+                <div className='btn-class'>
+                    <button onClick={clickHandler} className='btn ms-3 my-1 ' id='button'><MenuIcon height={25} width={25}></MenuIcon></button>
+                </div>
+                <div id='navManu'>
                     <div className='nav-hide'><Link className='links' to="/home">HOME</Link></div>
                     <div className='nav-hide'><Link className='links' to="/reviews">REVIEWS</Link></div>
                     <div className='nav-hide'><Link className='links' to="/dashboard">DASHBOARD</Link></div>
                     <div className='nav-hide'><Link className='links' to="/blogs">BLOGS</Link></div>
                     <div className='nav-hide'><Link className='links' to="/about">ABOUT</Link></div>
-                </div>
-                <div>
                 </div>
             </nav>
         </div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className=' container my-5'>
+            <Row>
+                <Col sm={12} lg={5} >
+                    <div className=' m-2'>
+                        <h3 className=' fw-bold'><span className='blue-part'>Play!</span>  Simulate!<span className='red-part'> Explore!</span></h3>
+                        <h1 className=' fw-bold'>Nintendo Switch</h1><br></br>
+                        <p className=' text-start'>Nintendo presents you with its latest technology Nintendo Switch.It will change your thoughts and reniew your experience in RPG Gaming with games like Pokemon,Castelvenia and many more.</p><br></br>
+                        <button></button>
+                    </div>
+                </Col>
+                <Col sm={12} lg={7}>
+                    <div className="m-2">
+                      <img className=' img-fluid' src="https://assets.nintendo.com/image/upload/ncom/en_US/switch/split-cta-system-mobile" alt="" />
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 };
