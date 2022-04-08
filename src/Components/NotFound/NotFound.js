29dcd562185cecbca7329cedf39436253dcdec6b
Tanvir Taushif <tanvirpial9961@gmail.com>
2022-04-08 18:05:44 +0600

    404 Not Found Commit

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Not Found</h1>
+        <div className='col-12'>
+            <img src="https://softzar.com/wp-content/uploads/2021/07/404-error-page-1024x566.png" alt="" className='not-found-img'/>
         </div>
     );
 };
