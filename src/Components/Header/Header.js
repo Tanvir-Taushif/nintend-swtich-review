63a630bb757ec0f19da07c095b275158feefe635
Tanvir Taushif <tanvirpial9961@gmail.com>
2022-04-09 19:47:41 +0600

    All Done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,17 +13,32 @@ const Header = () => {
         }
     }
     return (
-        <div>
-            <nav id='NavBar' className='my-4 d-flex justify-content-center '>
-                <div className='btn-class'>
-                    <button onClick={clickHandler} className='btn ms-3 my-1 ' id='button'><MenuIcon height={25} width={25}></MenuIcon></button>
-                </div>
-                <div id='navManu'>
-                    <div className='nav-hide'><CustomLink className='links' to="/home">HOME</CustomLink></div>
-                    <div className='nav-hide'><CustomLink className='links' to="/reviews">REVIEWS</CustomLink></div>
-                    <div className='nav-hide'><CustomLink className='links' to="/dashboard">DASHBOARD</CustomLink></div>
-                    <div className='nav-hide'><CustomLink className='links' to="/blogs">BLOGS</CustomLink></div>
-                    <div className='nav-hide'><CustomLink className='links' to="/about">ABOUT</CustomLink></div>
+        <div className='container text-center my-3'>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#"><h1 className='fw-bold'>Nintendo <span className='header'>Switch</span></h1></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse d-fle" id="navbarNavDropdown">
+                     <div className="navbar-nav">
+                        <div className="nav-item">
+                         <div className='nav-link'><CustomLink className='links' to="/home">HOME</CustomLink></div>
+                        </div>
+                        <div className="nav-item">
+                         <div className='nav-link'><CustomLink className='links' to="/reviews">REVIEWS</CustomLink></div>
+                        </div>
+                        <div className="nav-item">
+                         <div className='nav-link'><CustomLink className='links' to="/dashboard">DASHBOARD</CustomLink></div>
+                        </div>
+                        <div className="nav-item">
+                        <div className='nav-link'><CustomLink className='links' to="/blogs">BLOGS</CustomLink></div>
+                        </div>
+                        <div className="nav-item">
+                        <div className='nav-link'><CustomLink className='links' to="/about">ABOUT</CustomLink></div>
+                        </div>
+                     </div>
+                    </div>
                 </div>
             </nav>
         </div>
