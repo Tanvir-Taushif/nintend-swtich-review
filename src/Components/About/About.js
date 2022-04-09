3f9065439ed6d8bab673cf0ed3067ecee50a7b70
Tanvir Taushif <tanvirpial9961@gmail.com>
2022-04-09 13:50:27 +0600

    Blog Commit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './About.css';
 
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
+            <Container className='my-5'>
+                <Row>
+                    <Col sm={12} lg={6}>
+                        <div className='my-2'>
+                            <h5 className='fw-bold text-center'>Providing the best Gaming Experience</h5>
+                            <img className='about-img' src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/UpSwitch.jpeg" alt="" />
+                        </div>
+                    </Col>
+                    <Col sm={12} lg={6}>
+                        <div className='my-2'>
+                            <h5 className='fw-bold text-center'>Portable & Dynamic</h5>
+                            <img className='about-img' src="https://www.nintendo.com/sg/switch/index/img/hero_top_sp.jpg" alt="" />
+                        </div>
+                    </Col>
+                </Row>
+                
+            </Container>
         </div>
     );
 };
--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Blogs.css';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>Blogs</h1>
+        <div className='container'>
+            <h2 className='my-5 text-center fw-bold'>Nintendo Blogspot</h2>
+            <div className="blog-box">
+                <div className='p-2'>
+                    <h4 className='fw-bold'>What is Context API?</h4>
+                    <p>A React app can use the React Context API to generate global variables that can be passed around. It allows you to share unique information and aids in the resolution of prop-drilling issues at all levels of your application. It lets you to easily and lightly communicate state across the entire program (or a portion of it). We can define numerous unconnected contexts (stores) and use each in its own area in the program using the Context API. Context allows components to communicate values without having to pass a prop through each level of the tree directly.</p>
+                </div>
+            </div>
+            <div className="blog-box">
+                <div className='p-2'>
+                    <h4 className='fw-bold'>What is Semantic Tag?</h4>
+                    <p>Semantic HTML elements are those that express their meaning in a way that is both human and machine readable. &lt;header&gt; , &lt;footer&gt;, and &lt;article&gt; elements are all deemed semantic since they accurately explain the element's purpose and the sort of material it contains. It's a lot simpler to read now. It is more easily accessible. Additionally, semantic aspects contribute to more consistent code.</p>
+                </div>
+            </div>
         </div>
     );
 };
